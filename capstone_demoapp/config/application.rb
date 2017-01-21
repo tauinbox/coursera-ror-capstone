d575c545fe6e777072e198d4d68b348e55f551be
Alexander Tupavov <tauinbox@gmail.com>
2017-01-21 16:19:30 +0300

    add default orm option

--- capstone_demoapp/config/application.rb
+++ capstone_demoapp/config/application.rb
@@ -30,7 +30,12 @@ module CapstoneDemoapp
     # config.i18n.default_locale = :de
 
     # bootstraps mongoid within applications -- like rails console
-    Mongoid.load!('./config/mongoid.yml')    
+    Mongoid.load!('./config/mongoid.yml')
+    # which default ORM are we using with scaffold
+    # add --orm mongoid, or active_record to rails generate cmd line to be specific
+    config.generators { |g| g.orm :active_record }
+    # config.generators { |g| g.orm :mongoid }
+
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
