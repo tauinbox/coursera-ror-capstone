161d594813c2b098cd15591359dcb6b738157eb4
Alexander Tupavov <tauinbox@gmail.com>
2017-01-21 13:55:34 +0300

    refactored, added environment variables

--- provision.sh
+++ provision.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# The output of all these installation steps is noisy. With this utility
-# the progress report is nice and concise.
-
 function install {
-  dpkg -s $1 &>/dev/null 2>&1  && echo $1 already exists || {
+  dpkg -s $1 &>/dev/null 2>&1 && echo $1 already exists || {
     echo installing $1
     shift
     apt-get -y install "$@" >/dev/null 2>&1
@@ -33,26 +30,33 @@ apt-get install Ruby ruby2.3 ruby2.3-dev >/dev/null 2>&1
 update-alternatives --set ruby /usr/bin/ruby2.3 >/dev/null 2>&1
 update-alternatives --set gem /usr/bin/gem2.3 >/dev/null 2>&1
 
-echo installing Rails
-gem install rails -v 4.2.6 --no-ri --no-doc >/dev/null 2>&1
+type rails >/dev/null 2>&1 && echo "Rails already exists" || {
+  echo installing Rails
+  gem install rails -v 4.2.6 --no-ri --no-doc >/dev/null 2>&1
+}
 
-echo installing Rails-API
-gem install rails-api >/dev/null 2>&1
+type rails-api >/dev/null 2>&1 && echo "Rails-API already exists" || {
+  echo installing Rails-API
+  gem install rails-api >/dev/null 2>&1
+}
 
 echo installing Bundler
 gem install bundler --no-ri --no-doc >/dev/null 2>&1
 
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash - >/dev/null 2>&1
 install NodeJs nodejs
-install NPM npm
 
 install ImageMagic imagemagick
 
-echo installing Bower
-npm install bower -g >/dev/null 2>&1
+type bower >/dev/null 2>&1 && echo "Bower already exists" || {
+  echo installing Bower
+  npm install bower -g >/dev/null 2>&1
+}
 
-echo installing Gulp
-npm install gulp -g >/dev/null 2>&1
+type gulp >/dev/null 2>&1 && echo "Gulp already exists" || {
+  echo installing Gulp
+  npm install gulp -g >/dev/null 2>&1
+}
 
 type psql >/dev/null 2>&1 && echo "PostgreSQL already exists" || {
   install PostgreSQL postgresql postgresql-contrib libpq-dev
@@ -63,5 +67,20 @@ type psql >/dev/null 2>&1 && echo "PostgreSQL already exists" || {
 
 # Needed for docs generation.
 update-locale LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8
+
+# add environment variables
+declare file="/etc/environment"
+declare mask="POSTGRES"
+
+declare file_content=$( cat "${file}" )
+if [[ " $file_content " =~ $mask ]] # please note the space before and after the file content
+  then
+    echo "environment variables already set up"
+  else
+    echo "setting environment variables"
+    echo POSTGRES_USER=vagrant >> /etc/environment
+    # echo POSTGRES_PASSWORD=vagrant >> /etc/environment
+    # echo POSTGRES_HOST=localhost >> /etc/environment
+fi
 
 echo "Let's rock!"
