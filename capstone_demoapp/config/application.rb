1950f9ea1c5b88f5c7a5d1d5d80dc099ac266c85
Alexander Tupavov <tauinbox@gmail.com>
2017-01-21 16:10:44 +0300

    bootstraps mongoid within applications

--- capstone_demoapp/config/application.rb
+++ capstone_demoapp/config/application.rb
@@ -29,6 +29,9 @@ module CapstoneDemoapp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # bootstraps mongoid within applications -- like rails console
+    Mongoid.load!('./config/mongoid.yml')    
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
