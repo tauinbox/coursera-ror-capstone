72107d2cbd26be0f120aaf20489c1dd3afe79680
Alexander Tupavov <tauinbox@gmail.com>
2017-01-24 17:22:13 +0300

    populate db

--- m1-assignment/db/seeds.rb
+++ m1-assignment/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+City.create({ name: 'Baltimore' })
+State.create({ name: 'Maryland' })
