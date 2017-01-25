05c9e15224efdfcfa9c663f9b7591653b8d797c2
Alexander Tupavov <tauinbox@gmail.com>
2017-01-25 16:26:31 +0300

    server-pages

--- assignments/app/controllers/application_controller.rb
+++
@@ -1,4 +0,0 @@
-class ApplicationController < ActionController::API
-  # make the connection between controller action and associated view
-  include ActionController::ImplicitRender
-end
--- assignments/app/controllers/ui_controller.rb
+++
@@ -1,2 +0,0 @@
-class UiController < ApplicationController
-end
