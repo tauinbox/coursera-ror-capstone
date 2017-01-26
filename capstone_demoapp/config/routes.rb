5ff0acb240e4352753bebeead258daff1a117fe7
Alexander Tupavov <tauinbox@gmail.com>
2017-01-26 15:13:46 +0300

    re-added ui as root URL and merged AP+ext

--- capstone_demoapp/config/routes.rb
+++ capstone_demoapp/config/routes.rb
@@ -6,10 +6,10 @@ Rails.application.routes.draw do
 
   get "/client-assets/:name.:format", :to => redirect("/client/client-assets/%{name}.%{format}")
 
-  # this is another way how we can redirect all requests to the main index page (the first one was commented out in ui_controller's index method)
-  get "/", :to => redirect("/client/index.html")
+  # # this is another way how we can redirect all requests to the main index page (the first one was commented out in ui_controller's index method)
+  # get "/", :to => redirect("/client/index.html")
 
-  # get '/ui' => 'ui#index'
-  # get '/ui#' => 'ui#index'
-  # root 'ui#index'
+  get '/ui' => 'ui#index'
+  get '/ui#' => 'ui#index'
+  root 'ui#index'
 end
