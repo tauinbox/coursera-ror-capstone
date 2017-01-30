fe2e85f412567c3d2f75aff14c9bff2efd789b37
Alexander Tupavov <tauinbox@gmail.com>
2017-01-30 10:08:30 +0300

    configure manifest files

---
+++ assignments/app/assets/javascripts/tourist-app.js
@@ -0,0 +1,15 @@
+// Tourist App Javascript Manifest File
+//= require jquery2
+//= require bootstrap
+//= require angular
+//= require angular-ui-router
+//= require angular-resource
+
+//= require tourist-app/app.module
+//= require tourist-app/app.router
+//= require tourist-app/app.constant
+
+//= require tourist-app/cities/cities.module
+//= require tourist-app/cities/cities.service
+//= require tourist-app/cities/cities.controller
+//= require tourist-app/cities/cities.directive
---
+++ assignments/config/initializers/assets.rb
@@ -0,0 +1,2 @@
+Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( tourist-app.js tourist-app.css )
