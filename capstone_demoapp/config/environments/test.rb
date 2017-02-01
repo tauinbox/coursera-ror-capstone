0af1ea151bc66e796803552d2e76698d294de94e
Alexander Tupavov <tauinbox@gmail.com>
2017-02-01 14:16:17 +0300

    logger output level

--- capstone_demoapp/config/environments/test.rb
+++ capstone_demoapp/config/environments/test.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # add advanced log output
+  # Rails.logger = Logger.new(STDOUT)
+
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
@@ -40,6 +43,4 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  # add advanced log output
-  Rails.logger = Logger.new(STDOUT)
 end
