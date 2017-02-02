6b06d59817b23ba932e6b48a80092c0919c96cf3
Alexander Tupavov <tauinbox@gmail.com>
2017-02-02 08:45:01 +0300

    correct migrations (used ones from main branch for consistency)

--- assignments/db/migrate/20161222170032_add_foo_name_constraint.rb
+++
@@ -1,8 +0,0 @@
-class AddFooNameConstraint < ActiveRecord::Migration
-  def up
-    change_column :foos, :name, :string, {null: false}
-  end
-  def down
-    change_column :foos, :name, :string, {null: true}
-  end
-end
