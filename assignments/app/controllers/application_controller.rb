6eb3e5c6827f38b8c536d85445d35753426707a7
Alexander Tupavov <tauinbox@gmail.com>
2017-02-03 09:03:52 +0300

    added application solutions

--- assignments/app/controllers/application_controller.rb
+++ assignments/app/controllers/application_controller.rb
@@ -5,12 +5,22 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   rescue_from Mongoid::Errors::DocumentNotFound, with: :record_not_found
 
+  rescue_from ActiveRecord::StatementInvalid, with: :statement_invalid
+
   protected
     def record_not_found(exception) 
       payload = {
         errors: { full_messages:["cannot find id[#{params[:id]}]"] }
       }
       render :json=>payload, :status=>:not_found
+      Rails.logger.debug exception.message
+    end
+
+    def statement_invalid(exception)
+      payload = {
+        errors: { full_messages: exception.message.split("\n") }
+      }
+      render json: payload, status: :unprocessable_entity
       Rails.logger.debug exception.message
     end
 end
