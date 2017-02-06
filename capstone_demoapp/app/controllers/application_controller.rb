8265fb3c55abd0ff968c3b6f13be809fd7750d55
Alexander Tupavov <tauinbox@gmail.com>
2017-02-06 07:16:48 +0300

    update tests

--- capstone_demoapp/app/controllers/application_controller.rb
+++ capstone_demoapp/app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::API
-  # make the connection between controller action and associated view
+  #make the connection between controller action and associated view
   include ActionController::ImplicitRender
 
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-  rescue_from Mongoid::Errors::DocumentNotFound, with: :record_not_found  
+  rescue_from Mongoid::Errors::DocumentNotFound, with: :record_not_found
+
+  rescue_from ActiveRecord::StatementInvalid, with: :statement_invalid
 
   protected
     def record_not_found(exception) 
@@ -12,5 +14,13 @@ class ApplicationController < ActionController::API
       }
       render :json=>payload, :status=>:not_found
       Rails.logger.debug exception.message
-    end  
+    end
+
+    def statement_invalid(exception)
+      payload = {
+        errors: { full_messages: exception.message.split("\n") }
+      }
+      render json: payload, status: :unprocessable_entity
+      Rails.logger.debug exception.message
+    end
 end
--- capstone_demoapp/spec/factories/foobars.rb
+++ capstone_demoapp/spec/factories/foobars.rb
@@ -1,3 +1,17 @@
+class CustomSequence
+  def initialize(max)
+    @value, @max = 0, max
+  end
+
+  def next
+    @value = @value >= @max ? 0 : (@value + 1)
+  end
+
+  def peek
+    @value.to_s
+  end
+end
+
 FactoryGirl.define do
 
   factory :foo_fixed, class: 'Foo' do
@@ -26,6 +40,12 @@ FactoryGirl.define do
     name { Faker::Name.name }
   end
 
+  factory :foo_faker_num, class: 'Foo' do
+    sequence(:name, CustomSequence.new(9)) do |n|
+      Faker::Name.name + "#{n}"
+    end
+  end
+
   factory :foo_ctor, class: 'Foo' do
     transient do
       hash {}
@@ -33,7 +53,7 @@ FactoryGirl.define do
     initialize_with { Foo.new(hash) }
   end
 
-  factory :foo, :parent=>:foo_faker do
+  factory :foo, :parent=>:foo_faker_num do
   end
 
   factory :bar do
