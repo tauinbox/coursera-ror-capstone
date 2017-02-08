92b294d63c5eb486f46396875ab94990b60d8f78
Alexander Tupavov <tauinbox@gmail.com>
2017-02-08 17:41:30 +0300

    fix hashbang problem

--- capstone_demoapp/spec/features/manage_foos_spec.rb
+++ capstone_demoapp/spec/features/manage_foos_spec.rb
@@ -12,7 +12,7 @@ RSpec.feature "ManageFoos", type: :feature, :js=>true do
     let(:foos) { (1..5).map{ FactoryGirl.create(:foo) }.sort_by {|v| v["name"]} }
 
     scenario "when no instances exist" do
-      visit "#{root_path}/#/"
+      visit "#{root_path}/#!/"
       within(:xpath,FOO_LIST_XPATH) do     #<== waits for ul tag
         expect(page).to have_no_css("li")  #<== waits for ul li tag
         expect(page).to have_css("li", count:0) #<== waits for ul li tag
@@ -21,7 +21,7 @@ RSpec.feature "ManageFoos", type: :feature, :js=>true do
     end
 
     scenario "when instances exist" do
-      visit "#{root_path}/#/" if foos   #need to touch collection before hitting page
+      visit "#{root_path}/#!/" if foos   #need to touch collection before hitting page
       within(:xpath,FOO_LIST_XPATH) do
         expect(page).to have_css("li:nth-child(#{foos.count})") #<== waits for li(5)
         expect(page).to have_css("li", count:foos.count)        #<== waits for ul li tag
@@ -35,7 +35,7 @@ RSpec.feature "ManageFoos", type: :feature, :js=>true do
 
   feature "add new Foo" do
     background(:each) do
-      visit "#{root_path}/#/"
+      visit "#{root_path}/#!/"
       expect(page).to have_css("h3", text:"Foos") #on the Foos page
       within(:xpath,FOO_LIST_XPATH) do
         expect(page).to have_css("li", count:0)      #nothing listed
@@ -46,7 +46,7 @@ RSpec.feature "ManageFoos", type: :feature, :js=>true do
       expect(page).to have_css("label", :text=>"Name:")
       expect(page).to have_css("input[name='name'][ng-model*='foo.name']")
       expect(page).to have_css("button[ng-click*='create()']", :text=>"Create Foo")
-      expect(page).to have_button("Create Foo")
+      expect(page).to have_button("Create Foo", disabled: true)
     end
 
     scenario "complete form" do
@@ -120,6 +120,19 @@ RSpec.feature "ManageFoos", type: :feature, :js=>true do
         expect(page).to have_no_css("a",text:foo_state[:name])
       end
     end
-
   end
+
+  feature "Create button disabled when no name" do
+    scenario "disabled when no name" do
+      visit "#{root_path}/#!/"
+      within(:xpath,FOO_FORM_XPATH) do
+        expect(page).to have_selector("input", text: "")
+        expect(page).to have_css("input.ng-invalid")
+        expect(page).to have_css("button[disabled='disabled']")
+        expect(page).to have_css("button[ng-disabled*='$invalid']")
+        # expect(page).to have_button('Create Foo', disabled: true)
+      end      
+    end
+  end
+
 end
