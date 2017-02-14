63115debdc9e3f9ec1acd741544dc933393b882b
Alexander Tupavov <tauinbox@gmail.com>
2017-02-14 11:15:13 +0300

    fix error description

--- assignments/spec/features/authns_spec.rb
+++ assignments/spec/features/authns_spec.rb
@@ -50,8 +50,8 @@ RSpec.feature "Authns", type: :feature, :js=>true do
         expect(page).to have_css("#signup-form > span.invalid",
                                  :text=>"Password is too short")
         expect(page).to have_css("#signup-form > span.invalid",
-                                 :text=>"Email already in use")
-        expect(page).to have_css("#signup-email span.invalid",:text=>"already in use")
+                                 :text=>"Email has already been taken")
+        expect(page).to have_css("#signup-email span.invalid",:text=>"has already been taken")
         expect(page).to have_css("#signup-password span.invalid",:text=>"too short")
         within("#signup-password_confirmation") do
           expect(page).to have_css("span.invalid",:text=>"doesn't match")
